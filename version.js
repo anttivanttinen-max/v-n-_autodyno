8f837f0bd5ad0f212a2d6cbc8ef59f891b489dd2
anttivanttinen-max <antti.vanttinen@icloud.com>
2026-08-17 19:07:56 +0300

    Bump v34 rebuild after Service Worker fix

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-globalThis.MOTOLAB_RELEASE={version:"34.6-dev",label:"v34.6 DEV",build:"2026-08-17v-v34-rebuild"};
+globalThis.MOTOLAB_RELEASE={version:"34.6-dev",label:"v34.6 DEV",build:"2026-08-17w-v34-rebuild-swfix"};
