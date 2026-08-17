4d722f409ca0b4831065f78882e45b3757f848b7
anttivanttinen-max <antti.vanttinen@icloud.com>
2026-08-17 12:12:03 +0300

    Release MotoLab v33.6 MIC OFF authority

--- version.js
+++ version.js
@@ -1,2 +1,2 @@
 // Single release identity source for app shell and Service Worker.
-globalThis.MOTOLAB_RELEASE={version:"33.5",label:"v33.5",build:"2026-08-17j-beta-navigation"};
+globalThis.MOTOLAB_RELEASE={version:"33.6",label:"v33.6",build:"2026-08-17k-mic-off-authority"};
