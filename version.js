b0295528b848a8e7e39a9729111b1325ba639410
anttivanttinen-max <antti.vanttinen@icloud.com>
2026-08-17 13:31:47 +0300

    Set simple UI preview release identity

--- version.js
+++ version.js
@@ -1,2 +1,2 @@
 // Single release identity source for app shell and Service Worker.
-globalThis.MOTOLAB_RELEASE={version:"33.9",label:"v33.9",build:"2026-08-17n-owner-gear-beta"};
+globalThis.MOTOLAB_RELEASE={version:"34.0-rc1",label:"v34.0 RC1",build:"2026-08-17o-simple-user-ui-rc1"};
