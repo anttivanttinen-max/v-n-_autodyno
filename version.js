399e14de36ef51da630b8de59c3459b2e621eede
anttivanttinen-max <antti.vanttinen@icloud.com>
2026-08-17 16:56:06 +0300

    Release field-test recovery build

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-globalThis.MOTOLAB_RELEASE={version:"34.4",label:"v34.4",build:"2026-08-17s-auth-splash"};
+globalThis.MOTOLAB_RELEASE={version:"34.5",label:"v34.5",build:"2026-08-17t-field-test-recovery"};
