884a4bb9ff723f056b3cb418656bf134ccd739d0
anttivanttinen-max <antti.vanttinen@icloud.com>
2026-08-18 08:41:14 +0300

    Remove obsolete duplicated ty test copy

--- ty/scripts/patch_ui_build.js
+++
@@ -1,5 +0,0 @@
-const fs=require('fs');
-let s=fs.readFileSync('index.html','utf8');
-s=s.replace(/<script src="\.\/version\.js\?build=[^"]+"><\/script>/,'<script src="./version.js?build=2026-08-16i"></script>');
-s=s.replace(/navigator\.serviceWorker\.register\("\.\/sw\.js\?build=[^"]+"/g,'navigator.serviceWorker.register("./sw.js?build=2026-08-16i"');
-fs.writeFileSync('index.html',s);
