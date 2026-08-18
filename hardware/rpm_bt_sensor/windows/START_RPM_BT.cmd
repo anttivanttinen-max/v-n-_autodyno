f97649aa1d3c6874fbcf81860f7d3d151c4595b3
anttivanttinen-max <antti.vanttinen@icloud.com>
2026-08-18 21:12:30 +0300

    Add RPM-BT V1: START_RPM_BT.cmd

---
+++ hardware/rpm_bt_sensor/windows/START_RPM_BT.cmd
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+cd /d "%~dp0"
+where py >nul 2>nul || (echo Python 3 is required from python.org & pause & exit /b 1)
+if not exist ".venv\Scripts\python.exe" py -3 -m venv .venv || (pause & exit /b 1)
+".venv\Scripts\python.exe" -m pip install --disable-pip-version-check -q -r requirements.txt || (pause & exit /b 1)
+".venv\Scripts\python.exe" rpm_bt_logger.py %*
+if errorlevel 1 pause
+
+
