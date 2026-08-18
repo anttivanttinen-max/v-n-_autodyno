25bdb38230303f4328915e35b10100bb939df774
anttivanttinen-max <antti.vanttinen@icloud.com>
2026-08-18 10:14:25 +0300

    Bump v34.8 finalization build identity

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-globalThis.MOTOLAB_RELEASE={version:"34.7",label:"v34.7 BETA",build:"2026-08-18b-splash-run-analysis"};
+globalThis.MOTOLAB_RELEASE={version:"34.8",label:"v34.8 BETA",build:"2026-08-18c-final-ui-gear-auth"};
