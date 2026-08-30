e92d9064fad4930e3970f62227d8169007bdaecc
anttivanttinen-max <antti.vanttinen@icloud.com>
2026-08-30 22:39:40 +0300

    Load IMU auto-calibration module in MotorLab

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ const MODULES=[
  'gps_master_learning','raw_sync','dyno_curve_v2','ui_compact','default_dt','trip_research','research_sync',
  'user_identity','auth_session_guard','admin_pending_notify','password_login','user_features','feedback','community','merit','beta_menu','beta_release',
  'mic_authority','sensor_persistence','sensor_autostart','admin_test_tools','phone_rpm_smart','adaptive_rpm_learning',
- 'trip_phone_raw','trip_gear_guard','trip_gear_marker','live_status','live_status_guard',
+ 'imu_autocal_v1','trip_phone_raw','trip_gear_guard','trip_gear_marker','live_status','live_status_guard',
  'motorlab_branding','simple_user_ui','v34_dashboard_ui','motorlab_i18n','run_analysis_v34','v34_runtime_fixes','raw_data_ui_light'
 ];
 const STATIC=[
